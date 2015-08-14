299606aaae91bdf5a026e4d31ba10a39f8725d42
Hiten Jadeja <hitenjadeja@gmail.com>
2015-08-14 20:23:09 +0100

    first unit test class

--- stock_management/functional_tests.py
+++ stock_management/functional_tests.py
@@ -1,7 +1,21 @@
+import unittest
+
 from selenium import webdriver
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(2)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_get_home_page(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
