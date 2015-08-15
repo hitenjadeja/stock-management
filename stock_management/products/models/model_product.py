3a4651523a18b45d81759975f299b203074716a2
Hiten Jadeja <hitenjadeja@gmail.com>
2015-08-15 17:17:43 +0100

    add barcode field

--- stock_management/products/models/model_product.py
+++ stock_management/products/models/model_product.py
@@ -4,6 +4,7 @@ from django.db import models
 class Product(models.Model):
 
     name = models.CharField(max_length=150, unique=True)
+    barcode = models.IntegerField(blank=True, null=True)
 
     def __unicode__(self):
         return u'%s' % (self.name, )
