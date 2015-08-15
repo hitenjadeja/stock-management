adc8dce1e456976620887ac841f663c6f96aaff5
Hiten Jadeja <hitenjadeja@gmail.com>
2015-08-15 09:48:51 +0100

    Add web app and basic home page

--- stock_management/stock_management/settings.py
+++ stock_management/stock_management/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'products',
+    'web',
 )
 
 MIDDLEWARE_CLASSES = (
--- stock_management/stock_management/urls.py
+++ stock_management/stock_management/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'web.views.home_page', name='home'),
 ]
