27d05a451f46889bd413fbf5c228952102a105ad
Hiten Jadeja <hitenjadeja@gmail.com>
2015-08-15 16:14:03 +0100

    added warehouse products property

--- stock_management/products/models/model_warehouse.py
+++ stock_management/products/models/model_warehouse.py
@@ -4,3 +4,7 @@ from django.db import models
 class Warehouse(models.Model):
 
     name = models.CharField(max_length=150)
+
+    @property
+    def products(self):
+        return self.stock_set.all()
--- stock_management/products/tests/test_warehouse.py
+++ stock_management/products/tests/test_warehouse.py
@@ -1,11 +1,34 @@
 from django.test import TestCase
 
-from products.models import Warehouse
+from products.models import Product, Stock, Warehouse
 
 
 class WarehouseTest(TestCase):
+
+    def setUp(self):
+        self.product_1 = Product.objects.create(name="Product 1")
+        self.product_2 = Product.objects.create(name="Product 2")
+        self.warehouse = Warehouse.objects.create(name="Warehouse")
+
+        self.stock_1 = Stock.objects.create(
+            product=self.product_1,
+            warehouse=self.warehouse,
+            quantity=1,
+        )
+
+        self.stock_2 = Stock.objects.create(
+            product=self.product_2,
+            warehouse=self.warehouse,
+            quantity=1,
+        )
 
     def test_add_warehouse(self):
         warehouse = Warehouse.objects.create(name="Test")
 
         self.assertEqual(warehouse.name, "Test")
+
+    def test_list_warehouse_products(self):
+        self.assertListEqual(
+            list(self.warehouse.products),
+            [self.stock_1, self.stock_2],
+        )
